1c3a60bbf7eb9b21a72b397dda826edba09a08ea
volna <bohdanafanasyev@gmail.com>
2024-11-18 21:27:40 +0100

    Figure out color loss

--- App.vue
+++ App.vue
@@ -2,7 +2,7 @@
     <div class='h-screen w-full flex items-center justify-center py-20 px-40'>
         <NuxtImg
             class='h-full w-full object-cover fixed z-0'
-            src='/img/backgrounds/6.jpeg'
+            src='/img/backgrounds/1.jpeg'
         />
         <div class='z-10 relative glass-3d tint-3xdark w-full h-full rounded-3xl flex'>
             <UploadZone />
